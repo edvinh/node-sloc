0f988ddc3d3b23861b31fca353d285422f84f636
Edvin Havic <edvinhavic@gmail.com>
2019-03-11 12:17:15 +0100

    Formatting

--- sloc.js
+++ sloc.js
@@ -14,7 +14,7 @@ const utils = require('./utils')
  * @param  {Options} options           Options passed to the function.
  * @return {Promise}                   Resolves to an object containing SLOC and filepaths
  */
-const walkAndCount = (options) => {
+const walkAndCount = options => {
   return new Promise((resolve, reject) => {
     if (!fs.existsSync(options.path)) {
       reject('No such file or path. Exiting.')
@@ -25,40 +25,48 @@ const walkAndCount = (options) => {
       if (!utils.fileAllowed(options.path, options.extensions)) {
         resolve({ paths: [], sloc: {} })
       }
-      utils.countSloc(options.path).then((res) => {
+      utils.countSloc(options.path).then(res => {
         // If the path argument is a file, count it
-        resolve({ paths: [options.path], sloc: Object.assign({}, res, { files: 1, loc: res.sloc + res.comments }) })
+        resolve({
+          paths: [options.path],
+          sloc: Object.assign({}, res, { files: 1, loc: res.sloc + res.comments }),
+        })
       })
     } else if (fs.statSync(options.path).isDirectory()) {
-      utils.walk(options).then(res => {
-        let promises = []
-        let filteredPaths = utils.filterFiles(res, options.extensions)
-        filteredPaths.forEach(fpath => {
-          promises.push(utils.countSloc(fpath))
-        })
+      utils
+        .walk(options)
+        .then(res => {
+          let promises = []
+          let filteredPaths = utils.filterFiles(res, options.extensions)
+          filteredPaths.forEach(fpath => {
+            promises.push(utils.countSloc(fpath))
+          })
 
-        Promise.all(promises).then(values => {
-          let totSloc = 0
-          let totBlank = 0
-          let totComments = 0
-          let totFiles = filteredPaths.length
-          values.forEach((value) => {
-            totSloc += value.sloc
-            totBlank += value.blank
-            totComments += value.comments
-          })
-          resolve({
-            paths: filteredPaths,
-            sloc: {
-              loc: totSloc + totComments,
-              sloc: totSloc,
-              blank: totBlank,
-              comments: totComments,
-              files: totFiles,
-            },
-          })
-        }).catch(err => reject(`Error when walkning directory: ${err}`))
-      }).catch(err => reject(`Error when walkning directory: ${err}`))
+          Promise.all(promises)
+            .then(values => {
+              let totSloc = 0
+              let totBlank = 0
+              let totComments = 0
+              let totFiles = filteredPaths.length
+              values.forEach(value => {
+                totSloc += value.sloc
+                totBlank += value.blank
+                totComments += value.comments
+              })
+              resolve({
+                paths: filteredPaths,
+                sloc: {
+                  loc: totSloc + totComments,
+                  sloc: totSloc,
+                  blank: totBlank,
+                  comments: totComments,
+                  files: totFiles,
+                },
+              })
+            })
+            .catch(err => reject(`Error when walking directory: ${err}`))
+        })
+        .catch(err => reject(`Error when walking directory: ${err}`))
     }
   })
 }
