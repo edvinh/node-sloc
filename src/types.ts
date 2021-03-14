7dc3dc6c5729bf180d7aa4f737829a3971cea58f
Edvin Havic <edvinhavic@gmail.com>
2021-03-14 19:10:22 +0100

    Update type docs

--- src/types.ts
+++ src/types.ts
@@ -1,31 +1,54 @@
 export interface Options {
+  /**  The path to walk. */
   path: string
+
+  /** File extensions to look for. */
   extensions?: string[]
+
+  /**  A list of directories to ignore. */
   ignorePaths?: string[]
+
+  /** Whether or not to ignore the default extensions provided. */
   ignoreDefault?: boolean
+
+  /** Outputs extra information to this function if specified. */
   logger?: (log: string) => void
 }
 
 export type Callback = (err: Error | null, result: SLOCResult | null) => void
 
 export interface FileSLOC {
+  /** Lines of code. */
   loc: number
+
+  /** Source lines of code. */
   sloc: number
+
+  /** Number of blank lines. */
   blank: number
+
+  /** Number of lines of comments. */
   comments: number
 }
 
 export interface SLOC extends FileSLOC {
+  /** Number of files checked. */
   files: number
 }
 
 export interface SLOCResult {
+  /** Array of filepaths that have been checked. */
   paths: string[]
+
+  /** The resulting count for all paths. */
   sloc: SLOC
 }
 
 export interface FileExtension {
+  /** The file extension language. */
   lang: string
+
+  /** Comment syntax for the language. */
   comments: {
     line: string | null
     block: {
